a065809c0831daea9b893eac087748b29ac4fb5e
aniciagan <48643076+aniciagan@users.noreply.github.com>
2025-01-08 11:51:45 +0800

    Update home.tsx
    remove counter

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,26 +7,6 @@ export default function Home() {
     <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
       <h2 class="text-2xl">About me!</h2>
       <p class="mt-4">This is a page introducing myself and my skillsets</p>
-
-      <div class="flex items-center space-x-2 mt-4">
-        <button
-          type="button"
-          class="border rounded-lg px-2 border-slate-900"
-          onClick={() => setCount(count() - 1)}
-        >
-          -
-        </button>
-
-        <output class="p-10px">Count: {count()}</output>
-
-        <button
-          type="button"
-          class="border rounded-lg px-2 border-slate-900"
-          onClick={() => setCount(count() + 1)}
-        >
-          +
-        </button>
-      </div>
     </section>
   );
 }
