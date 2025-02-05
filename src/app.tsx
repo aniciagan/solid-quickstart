25ef65dd4e7bd4fbe64af76dc1137b20d03c1380
aniciagan <48643076+aniciagan@users.noreply.github.com>
2025-02-05 13:22:09 +0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -7,32 +7,31 @@ const App: Component = (props: { children: Element }) => {
   return (
     <div>
       {/* Updated Nav: Blue background and larger text */}
-    
-      <nav class="bg-blue-900 text-white px-6 py-2 shadow-lg transition-all duration-300 hover:bg-blue-800">
-        <ul class="flex items-center space-x-4">
-          <li class="flex items-center space-x-2">
-            <A href="/" class="no-underline hover:underline text-2xl">
-              Home
-            </A>
-          </li>
-          <li class="py-2 px-6">
-            <A href="/Analytics" class="no-underline hover:underline text-2xl">
-              Analytics
-            </A>
-          </li>
-          <li class="py-2 px-6">
-            <A href="/Documentations" class="no-underline hover:underline text-2xl">
-              Documentations
-            </A>
-          </li>
-          <li class="py-2 px-6">
-            <A href="/Reporting" class="no-underline hover:underline text-2xl">
-              Reporting
-            </A>
-          </li>
-          {/* Removed the URL input field */}
-        </ul>
-      </nav>
+    <nav class="bg-blue-900 text-white px-6 py-2 shadow-lg transition-all duration-300 hover:bg-blue-800">
+  <ul class="flex justify-evenly items-center w-full">
+    <li>
+      <A href="/" class="no-underline hover:underline text-lg">
+        Home
+      </A>
+    </li>
+    <li>
+      <A href="/Analytics" class="no-underline hover:underline text-lg">
+        Analytics
+      </A>
+    </li>
+    <li>
+      <A href="/Documentations" class="no-underline hover:underline text-lg">
+        Documentations
+      </A>
+    </li>
+    <li>
+      <A href="/Reporting" class="no-underline hover:underline text-lg">
+        Reporting
+      </A>
+    </li>
+  </ul>
+</nav>
+
 
       {/* Here we should render the children */}
       {props.children}
