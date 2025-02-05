ec1851b8dc7a208b787af400baef421652bd167e
aniciagan <48643076+aniciagan@users.noreply.github.com>
2025-02-05 12:39:11 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,19 @@
-import { createSignal } from "solid-js";
+import { createSignal, Suspense } from "solid-js";
 
 export default function Home() {
   const [count, setCount] = createSignal(0);
 
   return (
-    <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
-      <h2 class="text-2xl">About me!</h2>
-      <p class="mt-4">This is a page introducing myself and my skillsets</p>
-    </section>
+    <main class="max-w-2xl mx-auto h-screen flex flex-col justify-center px-4">
+      <h1 class="text-5xl mx-auto mb-8 text-slate-600 font-light">Welcome to my Page!</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* Children content would go here if you expect them */}
+      </Suspense>
+
+      <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
+        <h2 class="text-2xl">About me!</h2>
+        <p class="mt-4">This is a page introducing myself and my skillsets</p>
+      </section>
+    </main>
   );
 }
