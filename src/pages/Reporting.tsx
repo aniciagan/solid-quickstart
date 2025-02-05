378d3559e9e456169f57dd35cdaa259687458fe4
aniciagan <48643076+aniciagan@users.noreply.github.com>
2025-02-05 12:26:53 +0800

    Add files via upload

---
+++ src/pages/Reporting.tsx
@@ -0,0 +1,12 @@
+import { createSignal } from "solid-js";
+
+export default function Home() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
+      <h2 class="text-2xl">About me!</h2>
+      <p class="mt-4">This is a page introducing myself and my skillsets</p>
+    </section>
+  );
+}
