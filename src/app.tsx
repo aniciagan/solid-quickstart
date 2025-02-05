bbbfc4463fc0b1ef4b579f9c35b131a205c61624
aniciagan <48643076+aniciagan@users.noreply.github.com>
2025-02-05 13:24:07 +0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -7,32 +7,16 @@ const App: Component = (props: { children: Element }) => {
   return (
     <div>
       {/* Updated Nav: Blue background and larger text */}
-    <nav class="bg-blue-900 text-white px-6 py-2 shadow-lg transition-all duration-300 hover:bg-blue-800">
-  <ul class="flex justify-evenly items-center w-full">
-    <li>
-      <A href="/" class="no-underline hover:underline text-lg">
-        Home
-      </A>
-    </li>
-    <li>
-      <A href="/Analytics" class="no-underline hover:underline text-lg">
-        Analytics
-      </A>
-    </li>
-    <li>
-      <A href="/Documentations" class="no-underline hover:underline text-lg">
-        Documentations
-      </A>
-    </li>
-    <li>
-      <A href="/Reporting" class="no-underline hover:underline text-lg">
-        Reporting
-      </A>
-    </li>
-  </ul>
-</nav>
-
-
+      <nav class="bg-blue-900 text-white px-6 py-2 shadow-lg transition-all duration-300 hover:bg-blue-800">
+        <div class="max-w-5xl mx-auto">
+          <ul class="flex justify-between items-center w-full">
+            <li><A href="/" class="no-underline hover:underline text-lg">Home</A></li>
+            <li><A href="/Analytics" class="no-underline hover:underline text-lg">Analytics</A></li>
+            <li><A href="/Documentations" class="no-underline hover:underline text-lg">Documentations</A></li>
+            <li><A href="/Reporting" class="no-underline hover:underline text-lg">Reporting</A></li>
+          </ul>
+        </div>
+      </nav>
       {/* Here we should render the children */}
       {props.children}
     </div>
